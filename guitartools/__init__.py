fb2fbc0ffcf8512d4e348a5c3e7638af8b01a6ab
Ian Spielman <spielman@umd.edu>
2017-02-20 08:19:41 -0500

    Updated setup to be correct and added shell of timer function

---
+++ guitartools/__init__.py
@@ -0,0 +1,10 @@
+import configobj
+
+def timer(time):
+    """
+    runs a basic timer that delays for a time given by 
+    
+    time
+    """
+    
+    pass
--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@
 """
 Created on Fri Jul 10 08:27:47 2015
 
-complie via
-
-python setup.py build_ext --inplace
-
-DEBUG = 1 enables some debugging, but not in code that is pretty well tested
-DEBUG = 2 enables all debugging
-
 @author: ispielma
 
 """
@@ -18,34 +11,12 @@ try:
 except ImportError:
     from distutils.core import setup
 
-from Cython.Build import cythonize
-from numpy import get_include
-
 setup(
-    name="evolusim",
-    description='Digital evolution software',
+    name="guitartools",
+    description='Guitar Practice Tools',
     version='0.0.1rc0',
     author='Ian B. Spielman',
-    author_email='ian.spielman@nist.gov',
-    packages=['evolusim'],
-    install_requires=['ConfigObj', 'cymem'],
-    ext_modules = cythonize([
-        'evolusim/tools/c_identifier.pyx',
-        'evolusim/tools/c_llist.pyx',
-        'evolusim/tools/c_rand.pyx',
-        'evolusim/c_pool.pyx',
-        'evolusim/c_processor.pyx',
-        'evolusim/c_asm.pyx',
-        'evolusim/c_logger.pyx',
-        'evolusim/c_world.pyx'
-        ],
-        compiler_directives={
-                             'language_level': 3,
-                             'wraparound': False,
-                             'cdivision': True,
-                             'boundscheck': False
-                             },
-        compile_time_env={'DEBUG': 1}, gdb_debug=False
-        ),
-    include_dirs=[get_include()]
+    author_email='ian.spielman@gmail.com',
+    packages=['guitartools'],
+    install_requires=['ConfigObj'],
 )
