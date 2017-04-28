211927fad5460479e1ece48823554595d963e034
Ian Spielman <spielman@umd.edu>
2017-04-28 15:22:25 +0200

    Now have a metronome working!! This is really great so I do not need web access any more.

--- guitartools/__init__.py
+++ guitartools/__init__.py
@@ -20,8 +20,7 @@ import numpy as np
 
 import configobj
 import sys
-import threading
-import multiprocessing
+import os
 import time
 import random
 from pandas import DataFrame
@@ -30,6 +29,7 @@ from guitartools.timing_utils import base_timer
 
 try:
     import pygame.mixer, pygame.time, pygame.sndarray
+    
 except ImportError:
     print("ERROR: PyGame needed to run this program. \
           Please install it and try again")
@@ -258,70 +258,95 @@ class Changes():
 #
 # #############################################################################
 
-class Metronome():
-    """
-    Provides a user friendly metronome
-    """
-    
+class Metronome(object):
     def __init__(self):
-        self._timer = base_timer()
+        pygame.mixer.init()
 
-        self._worker = threading.Thread()
-                
-    def start(self, BPM=60.0, beats=4, keybeat=False):
+        self._started = False
+    
+        # Get open the sound file for the click and get a numpy array of its
+        # raw samples
+        # path = os.path.join(os.path.split(__file__)[0], "Click.wav")
+        #click = pygame.mixer.Sound(path)
+        #self._click_array = np.array(click)       
+        # click, self._click_array
+        # self._sample_dt = click.get_length()/self._click_array.shape[0]
+    
+        self._make_click()
+    
+    def _make_click(self):
+        inited = pygame.mixer.get_init()
+
+        # make a 0.01 s click
+        sample_rate = inited[0]
+        channels = inited[2]
+        # channels = pygame.mixer.get_num_channels()
+        duration = 0.01
+        samples = int(sample_rate*duration)
+        
+        time_array = np.linspace(0, duration, samples)
+        channels_array = np.zeros(channels)
+        time_array = np.meshgrid(channels_array, time_array)[1]
+        
+        sound_array = 2**15 * (np.sin(5*2*np.pi*time_array / duration) * 
+                       np.sin(np.pi*time_array / duration)**2 )
+                                    
+        sound_array = sound_array.astype(np.int16)
+        
+        sound = pygame.sndarray.make_sound(sound_array)
+        
+        self._sample_dt = sound.get_length()/sound_array.shape[0] 
+        self._click_array = sound_array
+    
+    def _generate_sound(self, bpm, emphasis=1):
+        beat_period = 60/bpm
+         
+        beat_nsamples = int(round(beat_period/self._sample_dt))
+        
+        # Truncate the audio of the click to ensure it's not longer than one
+        # beat. Crude way of making sure that when constructing the loop we
+        # don't write past the end of the array. Better approach would be to
+        # write whatever would go past the end of the array to the start of
+        # the array instead.
+        click_array = self._click_array[:beat_nsamples]
+        
+        # Make an array for audio long enough to contain the emphasis pattern:
+        loop_nsamples = int(round((emphasis * beat_nsamples)))
+        
+        loop_array = np.zeros((loop_nsamples,) + click_array.shape[1:],
+        dtype=click_array.dtype)
+        
+        
+        # First click emphasised:
+        loop_array[0:len(click_array)] += click_array
+        
+        # Remaining clicks not emphasised (here just done with a decrease in
+        # amplitude by a factor of 3):
+        for i in range(1, emphasis):
+            start_sample = i * beat_nsamples
+            stop_sample = start_sample + len(click_array)
+            loop_array[start_sample:stop_sample] += click_array//3
+        
+        # Make a pygame sound object ready for playing
+        self._clickloop = pygame.sndarray.make_sound(loop_array)
+    
+    def start(self, *args, **kwargs):
         """
-        Start a countdown for a time given by delay
-        
-        BPM: beats per minute
-        
-        beats: beats per measure
-        
-        keybeat: emhpasize the first beat of each measure
+        Start ticking.  Update to new settings if already ticking.  
         """
         
-        # end any running tasks
-        self.stop()
+        if self._started:
+            self.stop()
         
-        self._worker = threading.Thread(target=_metronome, args=(BPM, beats, keybeat, self._timer))
-        
-        self._worker.setDaemon(True)
-        self._worker.start()
+        # Start playing on a loop:
+        self._generate_sound(*args, **kwargs)
+        self._started = True
+        self._clickloop.play(-1)
     
     def stop(self):
-        """
-        Stop the metronome
-        """
-        
-        self._timer.stop()
-
-def _metronome(BPM, beats, keybeats, timer):
-    """
-    support function of self that is made its own thread for the metronome program
-    """
-    
-    #
-    # Start the metronome going!
-    #
-
-    interval = 60/BPM
-
-    timer.start(interval=interval)
-
-    i = 0
-    while timer.check():
-        try:
-            timer.countdown_queue.get(timeout=2*interval)
-        except:
-            pass
-        else:  
-            if keybeats and i % beats == 0:
-                sys.stdout.write('\a')
-            else:
-                sys.stdout.write('\a')
-            
-            sys.stdout.flush()
-                
-            i += 1
+        # Stop playing:
+        self._clickloop.stop()
+        self._started = False
 
 # #############################################################################
 #
